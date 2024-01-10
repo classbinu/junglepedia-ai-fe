1a1d6136c4bcba987882952aa47ff6112512a846
Classbinu <classbinu@gmail.com>
2024-01-10 13:56:08 +0900

    게시판 리스트 모바일 화면 넘침 버그 수정

--- components/post/postListCard.tsx
+++ components/post/postListCard.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-export function PostListCard({  post }) {
+export function PostListCard({ post }) {
   const truncateText = (text: string, limit: number) => {
     if (text.length > limit) {
       return text.substring(0, limit) + "...";
@@ -9,18 +9,20 @@ export function PostListCard({  post }) {
   };
 
   return (
-    <Link href={`/posts/${post.id}`}>
-      <div className="card w-full bg-base-100 shadow-xl m-2 hover:-translate-y-1 hover:shadow-2xl">
-        <div className="card-body py-4">
-          {post.isPrivate ? (
-            <div className="badge badge-accent">비공개</div>
-          ) : (
-            ""
-          )}
-          <h2 className="card-title">{post.title}</h2>
-          <p className="text-base-300">{truncateText(post.content, 100)}</p>
+    <div className="m-2">
+      <Link href={`/posts/${post.id}`}>
+        <div className="card w-full bg-base-100 shadow-xl hover:-translate-y-1 hover:shadow-2xl">
+          <div className="card-body py-4">
+            {post.isPrivate ? (
+              <div className="badge badge-accent">비공개</div>
+            ) : (
+              ""
+            )}
+            <h2 className="card-title">{post.title}</h2>
+            <p className="text-base-300">{truncateText(post.content, 100)}</p>
+          </div>
         </div>
-      </div>
-    </Link>
+      </Link>
+    </div>
   );
 }
